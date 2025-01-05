9a97baff221aa9cb19fd59e1d642b9096b21d774
MohammedMoataz <imohammedmoataz@gmail.com>
2025-01-05 04:59:31 +0200

    removed forgein keys and references and integrated swagger documnetation

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,23 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(helmet());
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('E-Commerce APIs Documentation')
+    .setDescription('The E-Commerce APIs Documentation')
+    .setVersion('1.0')
+    .addTag('e-commerce')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap();
