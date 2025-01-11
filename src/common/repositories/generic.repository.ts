4cdff6db2d8a6fc8accc04ae951dd53faeefe466
MohammedMoataz <imohammedmoataz@gmail.com>
2025-01-12 01:01:02 +0200

    refactoring

--- src/common/repositories/generic.repository.ts
+++ src/common/repositories/generic.repository.ts
@@ -3,7 +3,7 @@ import { UUID } from "crypto";
 export interface GenericRepository {
     create(data: any): Promise<any>;
     findAll(): Promise<any>;
-    findOne(id: number | UUID): Promise<any>;
+    findOneById(id: number | UUID): Promise<any>;
     update(id: number | UUID, data: any): Promise<any>;
     remove(id: number | UUID): Promise<any>;
 }
