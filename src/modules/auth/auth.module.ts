8b008cfa9c611234151e102808eb911254ed65c0
MohammedMoataz <imohammedmoataz@gmail.com>
2025-01-18 23:06:27 +0200

    reviewing modules

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,8 +4,8 @@ import { JwtModule } from '@nestjs/jwt';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { MailService } from 'src/common/helpers/mail/mail.service';
 import { UsersModule } from '../users/users.module';
+import { MailModule } from 'src/common/helpers/mail/mail.module';
 
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 
@@ -17,8 +17,9 @@ const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
       secret: ACCESS_TOKEN_SECRET,
       signOptions: { expiresIn: '1d' },
     }),
+    MailModule
   ],
-  providers: [AuthService, MailService],
+  providers: [AuthService],
   controllers: [AuthController],
   exports: [AuthService],
 })
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,17 +3,16 @@ import { Module } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { LoggerService } from 'src/common/helpers/logger/logger.service';
 import { CustomCacheModule } from 'src/common/helpers/cache/cache.module';
 import { CACHE_MANAGER, CacheModule } from '@nestjs/cache-manager';
+import { LoggerModule } from 'src/common/helpers/logger/logger.module';
 
 @Module({
-  imports: [CustomCacheModule],
+  imports: [CustomCacheModule, LoggerModule],
   controllers: [UsersController],
   providers: [
     UsersRepository,
     UsersService,
-    LoggerService,
     {
       provide: CACHE_MANAGER,
       useFactory: async () => CacheModule.register(), // Ensure CACHE_MANAGER is provided
