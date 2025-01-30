dc2cc895544b03fe8250bb24104473c6d75f393d
MohammedMoataz <imohammedmoataz@gmail.com>
2025-01-30 23:57:04 +0200

    enhanced cart controller and service

--- src/modules/cart/cart.controller.ts
+++ src/modules/cart/cart.controller.ts
@@ -12,34 +12,39 @@ import { ApiTags } from '@nestjs/swagger';
 import { CartService } from './cart.service';
 import { CreateCartDto } from './dto/create-cart.dto';
 import { UpdateCartDto } from './dto/update-cart.dto';
+import { Serialize } from 'src/common/interceptors/serialize.interceptor';
+import { CartDto } from './dto/cart.dto';
 
 @ApiTags('Cart APIs')
 @Controller('v1/carts/')
 export class CartController {
   constructor(private readonly cartService: CartService) { }
 
+  @Serialize(CartDto)
   @Post()
-  create(@Body() createCartDto: CreateCartDto) {
-    return this.cartService.create(createCartDto);
+  async create(@Body() createCartDto: CreateCartDto): Promise<CartDto> {
+    return await this.cartService.create(createCartDto);
   }
 
   @Get()
-  findAll() {
-    return this.cartService.findAll();
+  async findAll(): Promise<CartDto[]> {
+    return await this.cartService.findAll();
   }
 
+  @Serialize(CartDto)
   @Get(':id')
-  findOneById(@Param('id') id: number) {
-    return this.cartService.findOneById(id);
+  async findOneById(@Param('id') id: number): Promise<CartDto> {
+    return await this.cartService.findOneById(id);
   }
 
+  @Serialize(CartDto)
   @Patch(':id')
-  update(@Param('id') id: number, @Body() updateCartDto: UpdateCartDto) {
-    return this.cartService.update(id, updateCartDto);
+  async update(@Param('id') id: number, @Body() updateCartDto: UpdateCartDto): Promise<CartDto> {
+    return await this.cartService.update(id, updateCartDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: number) {
-    return this.cartService.remove(id);
+  async remove(@Param('id') id: number): Promise<string> {
+    return await this.cartService.remove(id);
   }
 }
--- src/modules/cart/cart.service.ts
+++ src/modules/cart/cart.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 import { CartRepository } from './cart.repository';
 import { CreateCartDto } from './dto/create-cart.dto';
 import { UpdateCartDto } from './dto/update-cart.dto';
+import { CartDto } from './dto/cart.dto';
+import { plainToClass } from 'class-transformer';
+import { Cart } from './entities/cart.entity';
 
 @Injectable()
 export class CartService {
@@ -10,25 +13,34 @@ export class CartService {
     private readonly cartRepository: CartRepository
   ) { }
 
-  async create(createCarDto: CreateCartDto) {
-    return this.cartRepository.create({
+  async create(createCarDto: CreateCartDto): Promise<CartDto> {
+    return await this.cartRepository.create({
       ...createCarDto,
     });
   }
 
   async findAll() {
-    return this.cartRepository.findAll();
+    const carts = await this.cartRepository.findAll();
+    let cartDto: CartDto[];
+
+    if (carts.length)
+      cartDto = carts.map((cart: typeof Cart) => plainToClass(CartDto, cart));
+
+    return cartDto;
   }
 
-  async findOneById(id: number) {
-    return this.cartRepository.findOneById(id);
+  async findOneById(id: number): Promise<CartDto> {
+    return await this.cartRepository.findOneById(id);
   }
 
-  async update(id: number, updateCartDto: UpdateCartDto) {
-    return this.cartRepository.update(id, updateCartDto);
+  async update(id: number, updateCartDto: UpdateCartDto): Promise<CartDto> {
+    return await this.cartRepository.update(id, updateCartDto);
   }
 
-  async remove(id: number) {
-    return this.cartRepository.remove(id);
+  async remove(id: number): Promise<string> {
+    const result = await this.cartRepository.remove(id);
+    return result.rowCount > 0
+      ? "Deleted successfully"
+      : "Deleted failed";
   }
 }
